bbb420682c365383d045a25a286f3a4e598c1fd2
byungmin12 <kbm940526@naver.com>
2022-03-06 23:48:47 +0900

    fix : change dataset = > e.target.closet().dataset

--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -19,7 +19,7 @@ export default function Nodes({ $app, initialState, onClick }) {
             node.type === "FILE"
               ? "../../assets/file.png"
               : "../../assets/directory.png";
-
+          console.log(node);
           return `
                     <div class="Node" data-node-id="${node.id}">
                         <img src="${iconPath}" />
@@ -32,17 +32,20 @@ export default function Nodes({ $app, initialState, onClick }) {
         ? `<div class="Node"><img src="../../assets/prev.png"/></div>${nodesTemplate}`
         : nodesTemplate;
     }
-  };
+    this.$target.querySelectorAll(".Node").forEach(($node) => {
+      $node.addEventListener("click", (e) => {
+        //e.target.dataset으로 위에서 data-로 시작하는 props에 접근할 수 있음
+        const { nodeId } = e.target.closest("[data-node-id]").dataset;
+        const selectedNode = this.state.nodes.find(
+          (node) => node.id === nodeId
+        );
 
-  this.$target.querySelectorAll(".Node").forEach(($node) => {
-    $node.addEventListener("click", (e) => {
-      //e.target.dataset으로 위에서 data-로 시작하는 props에 접근할 수 있음
-      const { nodeId } = e.target.dataset;
-      const selectedNode = this.state.nodes.find((node) => node.id === nodeId);
-
-      if (selectedNode) this.onClick(selectedNode);
+        if (selectedNode) {
+          this.onClick(selectedNode);
+        }
+      });
     });
-  });
+  };
 
   this.render();
 }
